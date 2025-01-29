b06bc5897b8d0d6020ae964fd27c93c26d4d07d5
Cartrius Phipps <cartriusphipps@gmail.com>
2025-01-29 10:45:53 -0600

    Change style of index.html - current top ratings

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const matchas = [
+    { name: "Matcha1", desc: "Matcha1 Description:", rating: 5, image: "images/matcha.jpg" },
+    { name: "Matcha2", desc: "Matcha2 Description:", rating: 4, image: "images/matcha.jpg" },
+    { name: "Matcha3", desc: "Matcha3 Description:", rating: 2, image: "images/matcha.jpg" }
+];
+
+const matchaList = document.getElementById("matcha-list");
+
+matchas.forEach(matcha => {
+    const card = document.createElement("div");
+    card.classList.add("matcha-card");
+    card.innerHTML = `<img src="${matcha.image}" alt="${matcha.name}">
+                    <div class="matcha-info">
+                        <h3>${matcha.name}</h3>
+                        <p>${matcha.desc}</p}
+                    </div>
+                    <div class="rating">${matcha.rating}</div>
+                `;
+    //console.log("hi")
+    matchaList.append(card);
+});
+
+
 document.addEventListener("DOMContentLoaded", function() {
     alert("Welcome to the Matcha Latte page!");
 });
